a005d5da1742f484119afd95f3f23a5d9b2c710f
lovesong7080 <lovesong7080@gmail.com>
2026-07-05 09:36:08 +0900

    Prepare RetirementCard layout

--- src/components/RetirementCard.jsx
+++ src/components/RetirementCard.jsx
@@ -1,41 +1,157 @@
-export default function RetirementCard(props) {
+import GrowthCard from "./GrowthCard";
+
+export default function RetirementCard({
+  yearsLeft,
+  monthlyGap,
+  neededRetirementAsset,
+  currentAsset,
+  shortageAsset,
+  monthlySavingNeed,
+  retirementProgress,
+  monthlyInvest,
+  setMonthlyInvest,
+  expectedReturn,
+  setExpectedReturn,
+  dividendYield,
+  setDividendYield,
+  inflationRate,
+  setInflationRate,
+  futureAssetManwon,
+  estimatedRetireYear,
+  yearlyDividend,
+  monthlyDividend,
+  dividendCoverage,
+  dividendGap,
+  asset10Year,
+  asset20Year,
+  asset30Year,
+  growthData,
+  yearDiff,
+}) {
   return (
     <div className="card">
       <h2>📊 은퇴 분석</h2>
 
       <div className="row">
         <span>은퇴까지</span>
-        <b>{props.yearsLeft}년</b>
+        <b>{yearsLeft}년</b>
       </div>
 
       <div className="row">
         <span>부족 생활비</span>
-        <b>{props.monthlyGap.toLocaleString()}만원/월</b>
+        <b>{monthlyGap.toLocaleString()}만원/월</b>
       </div>
 
       <div className="row">
         <span>필요 은퇴자산</span>
-        <b>{props.neededRetirementAsset.toLocaleString()}만원</b>
+        <b>{neededRetirementAsset.toLocaleString()}만원</b>
       </div>
 
       <div className="row">
         <span>현재 자산</span>
-        <b>{props.currentAsset.toLocaleString()}만원</b>
+        <b>{currentAsset.toLocaleString()}만원</b>
       </div>
 
       <div className="row">
         <span>부족한 자산</span>
-        <b>{props.shortageAsset.toLocaleString()}만원</b>
+        <b>{shortageAsset.toLocaleString()}만원</b>
       </div>
 
       <div className="row">
         <span>매월 필요한 투자금</span>
-        <b>{props.monthlySavingNeed.toLocaleString()}만원</b>
+        <b>{monthlySavingNeed.toLocaleString()}만원</b>
       </div>
 
-      <div className="row">
-        <span>목표 달성률</span>
-        <b>{props.retirementProgress}%</b>
+      <div style={{ marginTop: "18px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "6px",
+            fontWeight: "bold",
+          }}
+        >
+          <span>목표 달성률</span>
+          <span>{retirementProgress}%</span>
+        </div>
+
+        <div className="row">
+          <span>매월 투자</span>
+          <input
+            type="number"
+            value={monthlyInvest}
+            onChange={(e) => setMonthlyInvest(Number(e.target.value))}
+            style={{ width: "90px", textAlign: "right" }}
+          />
+        </div>
+
+        <div className="row">
+          <span>예상 수익률</span>
+          <input
+            type="number"
+            value={expectedReturn}
+            onChange={(e) => setExpectedReturn(Number(e.target.value))}
+            style={{ width: "90px", textAlign: "right" }}
+          />
+        </div>
+
+        <div className="row">
+          <span>배당수익률</span>
+          <input
+            type="number"
+            value={dividendYield}
+            onChange={(e) => setDividendYield(Number(e.target.value))}
+            style={{ width: "90px", textAlign: "right" }}
+          />
+        </div>
+
+        <div className="row">
+          <span>물가상승률</span>
+          <input
+            type="number"
+            value={inflationRate}
+            onChange={(e) => setInflationRate(Number(e.target.value))}
+            style={{ width: "90px", textAlign: "right" }}
+          />
+        </div>
+
+        <div className="row">
+          <span>예상 은퇴자산</span>
+          <b>{futureAssetManwon.toLocaleString()}만원</b>
+        </div>
+
+        <div className="row">
+          <span>예상 달성 연도</span>
+          <b>{estimatedRetireYear}년</b>
+        </div>
+
+        <div className="row">
+          <span>예상 연 배당금</span>
+          <b>{yearlyDividend.toLocaleString()}만원</b>
+        </div>
+
+        <div className="row">
+          <span>예상 월 배당금</span>
+          <b>{monthlyDividend.toLocaleString()}만원</b>
+        </div>
+
+        <div className="row">
+          <span>배당 생활비 충족률</span>
+          <b>{dividendCoverage}%</b>
+        </div>
+
+        <div className="row">
+          <span>배당 부족액</span>
+          <b>{dividendGap.toLocaleString()}만원/월</b>
+        </div>
+
+        <GrowthCard
+          asset10Year={asset10Year}
+          asset20Year={asset20Year}
+          asset30Year={asset30Year}
+          growthData={growthData}
+          yearDiff={yearDiff}
+        />
       </div>
     </div>
   );
